37a02a4153874ce8f5420d7917362f59c913bfae
Shelchin <hi@shelchin.com>
2025-08-12 11:33:28 +0800

    chore: Remove unused hooks files

--- src/hooks.ts
+++
@@ -1,4 +0,0 @@
-// @ts-expect-error - paraglide runtime module may not exist yet
-import { deLocalizeUrl } from '$lib/paraglide/runtime';
-
-export const reroute = (request: { url: URL | string }) => deLocalizeUrl(request.url).pathname;
